08b5ad11dffb7cd2173a536ae4241f6f683c68d2
byheart4u <abhishek.kumar2526@gmail.com>
2022-03-04 07:49:04 +0530

    fragment an portal added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import './App.scss';
+import Modal from './components/modal';
+
+const portalContainer = document.getElementById('portal');
 
 function App() {
   return (
-    <div className="App">
-      <h1>App Page</h1>
-    </div>
+    <Fragment>
+      {
+        ReactDOM.createPortal(<Modal />, portalContainer)
+      }
+      <div className="App">
+        <h1>App Page</h1>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/modal.tsx
@@ -0,0 +1,11 @@
+interface ModalProps {
+    
+}
+
+const Modal = () => {
+    return (
+        <h3>Simple Modat Wrapper Component</h3>
+    );
+}
+
+export default Modal;
